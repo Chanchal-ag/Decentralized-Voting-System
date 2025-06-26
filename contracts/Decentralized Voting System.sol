b1590fdbf17007556ee1e59ca36e083907d3c64d
Chanchal Agrawal <chanchalagrawal868@gmail.com>
2025-06-26 13:15:52 +0530

    Update Decentralized Voting System.sol

--- contracts/Decentralized Voting System.sol
+++ contracts/Decentralized Voting System.sol
@@ -1,1 +1,53 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Project {
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    address public admin;
+    mapping(address => bool) public voters;
+    mapping(uint => Candidate) public candidates;
+    uint public candidatesCount;
+    bool public electionStarted;
+
+    constructor() {
+        admin = msg.sender;
+        electionStarted = true;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only admin can perform this action");
+        _;
+    }
+
+    modifier electionOngoing() {
+        require(electionStarted, "Election is not active");
+        _;
+    }
+
+    function addCandidate(string memory _name) public onlyAdmin {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint _candidateId) public electionOngoing {
+        require(!voters[msg.sender], "You have already voted");
+        require(_candidateId > 0 && _candidateId <= candidatesCount, "Invalid candidate");
+
+        voters[msg.sender] = true;
+        candidates[_candidateId].voteCount++;
+    }
+
+    function endElection() public onlyAdmin {
+        electionStarted = false;
+    }
+
+    function getCandidate(uint _id) public view returns (Candidate memory) {
+        return candidates[_id];
+    }
+}
